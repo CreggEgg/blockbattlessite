4c209cc0999c23e4016bc41fb693f7f47e1660b6
CreggEgg <eggcregg@gmail.com>
2023-05-17 22:39:20 -0400

    i have to go to bed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,13 @@
 <script lang="ts">
 	import "../app.css";
-	let y: number = 0;
 </script>
 
-<h1 id="header">
-	<img id="icon" src="./icon.png" alt="icon" />
-	Block Battles
-</h1>
-<svelte:window bind:scrollY={y} />
-<slot />
-
-<style>
-	#header {
-		transition: font-size 0.2s ease-in-out 0s;
-
-		background-color: var(--secondary);
-		width: 100vw;
-		text-align: center;
-		margin-top: 0px;
-
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		position: sticky;
-		gap: 1rem;
-		font-size: 5rem;
-	}
-	#icon {
-		width: 4rem;
-		border-radius: 0.8rem;
-	}
-</style>
+<main>
+	<nav>
+		<a href="/join-discord">Join the Discord</a>
+		<a href="google.com">google</a>
+	</nav>
+	<div id="slot-div">
+		<slot />
+	</div>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,96 +1,83 @@
-<script lang="ts">
-	import LinkButton from "../components/LinkButton.svelte";
-</script>
-
-<main>
-	<p>
-		&nbsp;Welcome to Block Battles, the thrilling game that brings the
-		excitement of dueling to your fingertips! Inspired by the popular <a
-			target="_blank"
-			href="https://www.tiktok.com/tag/blockbattles">#blockbattles</a
-		> trend on TikTok, this game takes dueling to a whole new level, where strategy
-		and skill are paramount.
-	</p>
-	<p>
-		In Block Battles, you'll enter a dynamic world where players clash in
-		intense duels to earn each other's decks. With a complex and deep ruleset,
-		this game challenges your strategic thinking and decision-making abilities
-		in real-time battles. Are you ready to showcase your mastery of tactics and
-		claim victory in the arena?
-	</p>
-	<p>
-		Prepare to immerse yourself in a diverse and ever-expanding card collection.
-		Each card represents a unique character, creature, or spell, all beautifully
-		crafted in a blocky aesthetic. As you progress, you'll unlock new cards,
-		build powerful decks, and develop your own signature playstyle. Will you
-		rely on brute force, cunning strategy, or a combination of both?
-	</p>
-	<p>
-		To succeed in Block Battles, you must carefully construct your deck,
-		selecting the perfect combination of cards that synergize with your chosen
-		strategy. Craft devastating combos, unleash mighty spells, and summon
-		formidable creatures to overpower your opponents. The possibilities are
-		vast, and the choices you make will determine your success on the
-		battlefield.
-	</p>
-	<p>
-		Engage in exhilarating real-time duels against players from around the
-		world. Utilize your deck's strengths while exploiting your opponent's
-		weaknesses. With each move and countermove, you'll need to adapt your
-		strategy on the fly, predicting your opponent's next move and outsmarting
-		them at every turn. It's a game of wits and tactics where only the most
-		skilled duelists prevail.
-	</p>
-	<p>
-		But the thrill doesn't end with victory. When you defeat an opponent, you
-		have the opportunity to claim their deck as your own, adding their cards to
-		your collection. The more decks you acquire, the more diverse your
-		strategies become. Experiment with different combinations, test new tactics,
-		and evolve as a duelist to become unstoppable.
-	</p>
-	<p>
-		Block Battles fosters a vibrant and passionate community of players. Connect
-		with fellow duelists, share strategies, and learn from the best. Participate
-		in tournaments and events that push your skills to the limit, and climb the
-		ranks to prove your worth. Your name will be etched into the annals of Block
-		Battles history as a legendary duelist.
-	</p>
-	<p>
-		The game's developers are committed to delivering a rich and immersive
-		experience. Regular updates introduce new cards, game modes, and features,
-		ensuring that Block Battles always offers fresh challenges and surprises.
-		The journey to mastery is endless, and there's always something new to
-		discover.
-	</p>
-	<p>
-		Are you ready to enter the arena and rise to the top of the <a
-			target="_blank"
-			href="https://www.tiktok.com/tag/blockbattles">#blockbattles</a
-		>
-		leaderboard? Download the game now and embrace the thrilling world of dueling.
-		Prepare your deck, sharpen your mind, and get ready to outmaneuver your opponents
-		in the epic battles of Block Battles. Show the world your strategic prowess and
-		become the ultimate duelist!
-	</p>
-	<LinkButton href="/join-server" style="width: max-content;"
-		>Join the server</LinkButton
-	>
-	<LinkButton href="/join-discord" style="width: max-content;"
-		>Join the Discord</LinkButton
-	>
-</main>
+<article>
+	<div id="main">
+		<h1>BLOCK BATTLES</h1>
+		<h2>play.thisisatemplate.net</h2>
+		<div id="credits">
+			<img src="blitz.png" alt="blitz" />
+			<img src="spartan.png" alt="spartan" />
+			<img src="cregg.png" alt="cregg" />
+			<p class="name">Blitzminer</p>
+			<p class="name">Spartan</p>
+			<p class="name">Cregg</p>
+		</div>
+		<div id="links">
+			<a href="/join-discord" target="_blank">Discord</a><a
+				href="https://youtube.com"
+				target="_blank">Youtube</a
+			><a href="https://tiktok.com" target="_blank">TikTok</a>
+		</div>
+	</div>
+</article>
 
 <style>
-	main {
-		width: 80%;
+	article {
 		display: flex;
 		flex-direction: column;
-		padding-bottom: 5%;
 		align-items: center;
-		gap: 2rem;
+		justify-content: center;
+		text-align: center;
+		background-image: url("lobby.jpg");
+		min-height: 100%;
+		height: 100%;
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-position: center;
+		background-size: cover;
+	}
+	h1 {
+		font-size: 3rem;
+		margin-bottom: 0px;
 	}
 
-	main > p {
-		font-size: 3rem;
+	h2 {
+		margin-top: 0.5rem;
+	}
+	.name {
+		color: white;
+		font-weight: 700;
+		font-size: 1.5rem;
+		margin: 0px;
+	}
+	div#credits {
+		width: 60%;
+		display: grid;
+
+		grid-template-rows: 1fr 1fr;
+		grid-template-columns: 1fr 1fr 1fr;
+		place-items: center;
+		column-gap: 2rem;
+	}
+	div#credits > img {
+		width: 5rem;
+		image-rendering: pixelated;
+	}
+
+	#main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 60%;
+		height: 90%;
+		background-color: rgb(0, 0, 0, 0.65);
+		border-radius: 25px;
+	}
+
+	#links {
+		margin-bottom: 5rem;
+		margin-top: auto;
+		display: flex;
+		gap: 3rem;
+	}
+	#links > a {
 	}
 </style>
